dc4faea63c213517c5c290df83bf7fa27f49e5c5
ysukhost <ysukhost@PLNB06>
2022-08-04 16:25:41 +0200

    Add project files.

---
+++ Migraitons/DirectoryConfigurator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migraitons
+{
+    public class DirectoryConfigurator
+    {
+        public static void Replace(string filePath, string oldSubstring, string newSubstring)
+        {
+            //if (File.Exists(filePath) == false) return;
+
+            string input = File.ReadAllText(filePath).Replace(oldSubstring, newSubstring);
+
+            using (FileStream fstream = new(filePath, FileMode.Create))
+            {
+                byte[] buffer = Encoding.Default.GetBytes(input);
+                fstream.Write(buffer, 0, buffer.Length);
+            }
+        }
+        public static List<string> GetAllFiles(List<string> filePaths,  string path)
+        {
+            string[] files = Directory.GetFiles(path, "*.xml");
+
+            foreach (var file in files)
+            {
+                filePaths.Add(file);
+            }
+
+            string[] dirs = Directory.GetDirectories(path);
+
+            if (dirs.Length == 0)
+            {
+                return filePaths;
+            }
+            else
+            {
+                foreach (var dir in dirs)
+                {
+                    filePaths = GetAllFiles(filePaths, dir);
+                }
+
+                return filePaths;
+            }
+        }
+    }    
+}
---
+++ Migraitons/Program.cs
@@ -0,0 +1,28 @@
+﻿using Migraitons;
+
+string[] paths = {
+    @"D:\Desktop\C519 FFL\C519 FFL\Diagrams"
+    //@"D:\Desktop\98\HCM_Modules\Diagrams",
+    //@"D:\Desktop\98\Rework\Diagrams",
+    //@"D:\Desktop\98\Tesla Model Y RL\Diagrams",
+    //@"D:\Desktop\98\X540 DRL\Diagrams"
+ };
+
+foreach(var dirPath in paths)
+{
+    List<string> files = new List<string>();
+    DirectoryConfigurator.GetAllFiles(files, dirPath);
+
+    foreach (var file in files)
+    {
+        DirectoryConfigurator.Replace(file, "Read GV", "Read GVSr");
+        DirectoryConfigurator.Replace(file, "Write GV", "Write GVSr");
+        DirectoryConfigurator.Replace(file, "Poll GV", "Poll GVSr");
+        DirectoryConfigurator.Replace(file, "GVar", "GVarSr");
+    }
+
+    Console.WriteLine(dirPath + "\nFinished.");
+}
+
+Console.WriteLine("All finished.");
+
